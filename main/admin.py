02223e585afae4184271f459ac1266f20f0a0b6c
Kingjmk <jameelhamdan99@yahoo.com>
2019-06-18 18:13:18 +0300

    Models added
    + service and customer models and a many-many relation between them

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import customer
+from .models import service
 
 # Register your models here.
+admin.site.register(customer)
+admin.site.register(service)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class service(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):              
+        return self.name
+
+class customer(models.Model):
+    name = models.CharField(max_length=128)
+    phonenumber = models.CharField(max_length=10)
+    services = models.ManyToManyField(service, through='customerservice')
+
+    def __str__(self):              
+        return self.name
+
+class customerservice(models.Model):
+    customer = models.ForeignKey(customer, on_delete= models.CASCADE)
+    service = models.ForeignKey(service, on_delete= models.CASCADE)
+    active = models.BooleanField(default=True)
